cdd41eb98c4db23932665ae12153e1bc5b787365
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-12-12 11:07:11 +0200

    Подключает библиотеку simplelightbox и ее стили, а также иницилизирует ее

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './css/styles.css';
 const axios = require('axios');
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
