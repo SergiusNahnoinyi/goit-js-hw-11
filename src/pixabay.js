2dfaea251e5d9d094fa7064fdcda19d23019e1bf
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-12-12 13:32:33 +0200

    Указывает список параметров строки запроса и делает тестовый запрос

--- src/pixabay.js
+++ src/pixabay.js
@@ -1,1 +1,12 @@
+const API_KEY = '24778312-18f63a423fbed9787418fdc16';
+const BASE_URL = 'https://pixabay.com/api/';
+const searchParams = {
+  q: 'yellow+flowers',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+};
 
+const url = `${BASE_URL}?key=${API_KEY}`;
+
+fetch(url, searchParams).then(response => response.json());
